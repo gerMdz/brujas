d217a19113c51db1a753a19552f69b116efba501
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2024-03-25 08:41:16 -0300

    Agregando creación de cliente

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function resolveAuthorization()
+    {
+        if (auth()->user()->accessToken->expires_at <= now()) {
+            $response = Http::withHeaders([
+                'Accept' => 'application/json'
+            ])
+                ->post(config('services.apiconsume.cliente_oauth'), [
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => auth()->user()->accessToken->refresh_token,
+                    'client_id' => config('services.apiconsume.client_id'),
+                    'client_secret' => config('services.apiconsume.cliente_secret'),
+
+
+                ]);
+
+            $access_token = $response->json();
+
+            auth()->user()->accessToken->update([
+                'access_token' => $access_token['access_token'],
+                'refresh_token' => $access_token['refresh_token'],
+                'expires_at' => now()->addSecond($access_token['expires_in'])
+            ]);
+        }
+    }
+
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
@@ -10,6 +9,11 @@ class PostController extends Controller
 
     public function store()
     {
+
+$this->resolveAuthorization();
+
+
+        /** --------- */
         $headers = [
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . auth()->user()->accessToken->access_token];
@@ -26,14 +30,14 @@ class PostController extends Controller
             [
                 'Accept' => 'application/json',
                 'Authorization' => 'Bearer ' . auth()->user()->accessToken->access_token]
-            )
-        ->post($url, [
-            'nombre' => 'Nombre 1',
-            'urlLink' => 'link-1',
-            'resumen' => 'Post por cliente',
-            'body' => 'Todo es todo',
-            'categoria_id' => 1,
-        ]);
+        )
+            ->post($url, [
+                'nombre' => 'Nombre 1',
+                'urlLink' => 'link-2',
+                'resumen' => 'Post por cliente',
+                'body' => 'Todo es todo',
+                'categoria_id' => 1,
+            ]);
 
         return $response->json();
     }
